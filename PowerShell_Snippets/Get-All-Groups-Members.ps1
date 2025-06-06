3e0de6ef4f089bb0b63f784c53bfef2e39715761
Joshua McKeever <44627790+Joshua-McKeever@users.noreply.github.com>
2025-06-06 08:41:53 -0400

    Update Get-All-Groups-Members.ps1

--- PowerShell_Snippets/Get-All-Groups-Members.ps1
+++ PowerShell_Snippets/Get-All-Groups-Members.ps1
@@ -2,14 +2,11 @@ $TempFile = "C:\Downloads\All_Groups.csv"
 $OutFIle = "C:\Downloads\GroupMembers.csv"
 
 # Export list of all AD Groups
-Get-ADGroup -Filter * | Select DistinguishedName | Export-CSV $TempFile -NoTypeInformation
-
+Get-ADGroup -Filter "GroupCategory -eq 'Security'" | Select DistinguishedName | Export-CSV $TempFile -NoTypeInformation
 # Iterate through each group and extract active members
 $SourceGroups = Import-CSV $TempFile
 ForEach($SourceGroup in $SourceGroups){
-
     $CurrentGroup = $SourceGroup.DistinguishedName
     $LDAPfilter = "(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(memberOf=$CurrentGroup))"
-    Get-ADUser -LDAPFilter $LDAPfilter -SearchBase "DC=[your company],DC=org" | Select @{Name = "GroupDN"; Expression = {$CurrentGroup}}, SamAccountName, Company, physicalDeliveryOfficeName, Department, Title | Export-Csv $OutFIle -Append
-
+    Get-ADUser -LDAPFilter $LDAPfilter -SearchBase "DC=corp,DC=[your company],DC=org" -Properties Company, physicalDeliveryOfficeName, Department, Title | Select @{Name = "GroupDN"; Expression = {$CurrentGroup}}, SamAccountName, Company, physicalDeliveryOfficeName, Department, Title | Export-Csv $OutFIle -Append
 }
